af16f206711d50a18d15d91605c56b3ca022e0a0
Chibata1994 <chibatajimenez@gmail.com>
2023-06-17 14:16:19 -0500

    Ejercicios para la Misión 6 del BootCamp

---
+++ MISION_6/Operador_Ternario.js
@@ -0,0 +1,12 @@
+// SOLUCIÓN
+
+let number = 8;
+
+number == 8 ? console.log("El número es igual a 8") : console.log("No es 8");
+
+//EJERCICIO
+//*Crea un archivo llamado operador-ternario.js
+//*Escribir un programa usando el operador ternario
+//que imprima El número es igual a 8 si el número
+//asignado a la variable number es 8, e imprima No es 8
+//si es diferente de 8.
---
+++ MISION_6/Saludando.js
@@ -0,0 +1,18 @@
+
+// SOLUCIÓN:
+
+let time = 12;
+
+if (time < 12) {
+    console.log("Buenos Días");
+} else if (time >= 12, time <= 18) {
+    console.log("Buenas Tardes");
+} else {
+    console.log("Buenas Noches");
+}
+
+// EJERCICIO:
+//Crea un archivo llamado saludando.js
+//*Escribir un programa que imprima Buenos días si el número asignado a la
+//variable time es menor que 12. Si el numero es mayor o igual a 12 y menor o igual a 18,
+//el programa debe imprimir Buenas tardes, de lo contrario debe imprimir Buenas noches.
